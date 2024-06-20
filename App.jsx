2f8de80b178a26209a5f7ebf5b6e810b4f6894fe
AntonRodensjo <anton.rodensjo@hotmail.com>
2024-06-20 14:42:42 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import Checklist from './components/Checklist/Checklist.jsx' 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Checklist />
+    </>
+  )
+}
+
+export default App
